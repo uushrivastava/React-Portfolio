0367ab8bb61c9e3695ea0368810dae8e760a27ef
utkarsh shrivastava <shrivastava.utkarsh0@gmail.com>
2021-05-27 17:55:14 +0530

    app component changed to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
       hello world
     </div>
   );
+  }
 }
 
 export default App;
